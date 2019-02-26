8fdf8f2ca328206cbece6be499bfc2af07469d51
Paula Gonzalez <psgnzalez@gmail.com>
2019-02-26 18:59:55 -0300

    Adicionando primeira lista de exercícios

---
+++ Lista1/Aula1902/Aula1902/Aula1902/Ex01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1902
+{
+    class Ex01
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador";
+            string produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos: ");
+            Console.WriteLine("{0}, cujo preço é ${1:F2}", produto1, preco1);
+            Console.WriteLine("{0}, cujo preço é ${1:F2}", produto2, preco2);
+            Console.WriteLine("");
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine("");
+            Console.WriteLine($"Medida com oito casas decimais: {medida}");
+            Console.WriteLine("Arredondado (três casas decimais): {0}", Math.Round(medida, 3));
+            Console.WriteLine("Separador decimal invariant culture: {0}", Math.Round(medida, 3).ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
